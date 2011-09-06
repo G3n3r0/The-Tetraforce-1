960c4905c55b6b583dbece558b257d656218a669
g3n3r0 <colin.william.atkinson@gmail.com>
2011-09-06 17:35:38 +0000

    9/6/2011 1335

---
+++ README.md
@@ -0,0 +1,28 @@
+The Legend of Zelda: The Tetraforce Shatters
+============================================
+
+The Story of Link's Curse
+
+The Myth
+--------
+
+Before life began, before the world had form, three golden goddesses descended upon the chaotic land of Hyrule, they were Din, the goddess of power, Nayru, the goddess of wisdom and Farore, the goddess of courage.
+
+Din, with her strong flaming arms, cultivated the land to create the earth. Nayru poured her wisdom onto the earth to give the spirit of law to the world. Farore's rich soul created all life forms who would uphold the law.
+
+These three great goddesses returned to the heavens, leaving behind the golden sacred Triforce. Since then, the Triforce has become the basis for Hyrule's providence. Where the Triforce stood became sacred land...
+
+However, more powerful than these three, there was another goddess. She was the one who had created Din, Nayru, Farore and even the earth itself. She was nameless, for she needed no name. She was the goddess of time, as well as the goddess of both shadow and hope. As the other goddesses departed, leaving their essences, she placed a piece of hers, as well.
+
+Should someone obtain all four of these pieces, they would have ultimate power, being as powerful as the goddesses themselves, if not more so.
+
+Din, Nayru and Farore were angered by the other goddess' actions. They were worried about what would transpire should a mortal obtain all four of the pieces of the Tetraforce. Even just the fourth piece would be extremely powerful.
+
+However, the goddesses let the piece remain in Hyrule, hidden, like the others.
+
+Back Story
+----------
+
+Link is a young Hyrulian adventurer. However, he is quite greedy. He has heard the legends of the fourth piece of the Triforce, and wishes to obtain it for himself.
+
+Link has found three of the four pieces. He already has the powers of wisdom, courage and power. He has found the ancient temple of time, and hopes to find the final piece of the Tetraforce here.
--- Scripts/main.js
+++ Scripts/main.js
@@ -13,42 +13,78 @@ x$(window).on("load", function() {
         this.img = img;
         this.x = x;
         this.y = y;
-        this.fdata = {f:0,b:1,r:2,l:3};
-        this.sSheet = new SpriteSheet(this.img, 32, 32,this.fdata);
+        //this.fdata = {f:0,b:1,r:2,l:3};
+        this.fdata = {
+            b1:0,
+            b2: 1,
+            b3: 2,
+            r1: 3,
+            r2: 4,
+            r3: 5,
+            f1: 6,
+            f2: 7,
+            f3: 8,
+            l1: 9,
+            l2: 10,
+            l3: 11
+        };
+        this.sSheet = new SpriteSheet(this.img, 24, 24,this.fdata);
         this.bs = new BitmapSequence(this.sSheet);
         this.bs.y = this.y;
 	    this.bs.x = this.x;
-        this.bs.gotoAndStop("b");
+        this.bs.gotoAndStop("b2");
         stage.addChild(this.bs);
         stage.update();
+        this.step = 1;
         this.update = function(up,down,left,right,space) {
             if(up) {
                 //this.bit.y -= this.spd[1];
                 if(this.y>cs/100) {
                     this.y -= this.spd[1];
                 }
-                this.bs.gotoAndStop("b");
+                //console.log(this.step)
+                //this.bs.gotoAndStop("b2");
+                this.bs.gotoAndStop("b"+this.step);
+                this.step += 1;
+                if(this.step>3) {
+                    this.step = 1;
+                }
             }
             if(down) {
                 //this.bit.y += this.spd[1];
                 if(this.y<canvas.height-32-cs) {
                     this.y += this.spd[1];
                 }
-                this.bs.gotoAndStop("f");
+                //this.bs.gotoAndStop("f2");
+                this.bs.gotoAndStop("f"+this.step);
+                this.step += 1;
+                if(this.step>3) {
+                    this.step = 1;
+                }
             }
             if(right) {
                 //this.bit.x += this.spd[0];
                 if(this.x<canvas.width-32-cs) {
                     this.x += this.spd[0];
                 }
-                this.bs.gotoAndStop("r");
+                //this.bs.gotoAndStop("r2");
+                this.bs.gotoAndStop("r"+this.step);
+                this.step += 1;
+                if(this.step>3) {
+                    this.step = 1;
+                }
             }
             if(left) {
                 //this.bit.x -= this.spd[0];
                 if(this.x>cs) {
                     this.x -= this.spd[0];
                 }
-                this.bs.gotoAndStop("l");
+                //this.bs.gotoAndStop("l2");
+                this.bs.gotoAndStop("l"+this.step);
+                this.step += 1;
+                if(this.step>3) {
+                    this.step = 1;
+                }
             }
             this.bs.x = this.x;
             this.bs.y = this.y;
@@ -177,7 +213,7 @@ x$(window).on("load", function() {
         window.player = null;
         img = new Image();
         img.onload = function(e) {
-            console.log(maxY);
+            //console.log(maxY);
             window.player = new Player(canvas.width/2-16,canvas.height-32-cs,this);
             //window.player = new Player(canvas.width/2-16,maxY);
             Ticker.setFPS(12);
@@ -186,7 +222,7 @@ x$(window).on("load", function() {
         img.onerror = function(e) {
             console.log(e);
         };
-        img.src = "Graphics/joomba.png";
+        img.src = "Graphics/linkSpritesTry1-2.png";
     }
     init();
 });
